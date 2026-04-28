4febe864b4b67947c217cd1c9500ad843ddae84e
Himani Dadem <himanidadem@gmail.com>
2026-04-27 17:53:15 -0700

    Added .gitignore and Histograms of tweets

---
+++ SQL/easy/histograms_of_tweets.sql
@@ -0,0 +1,8 @@
+with counts as (SELECT user_id, count(*) as tweet_bucket
+FROM tweets
+where EXTRACT(YEAR FROM tweet_date)='2022'
+GROUP BY user_id)
+
+select tweet_bucket, count(*) as users_num
+from counts
+group by tweet_bucket
